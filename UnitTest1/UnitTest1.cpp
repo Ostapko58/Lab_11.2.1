2ee0e31df4cb8e63aac945ef2b71d1f226e22d34
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-04-25 18:50:06 +0300

    added files

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_11.2.1/lab_11.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Student stud;
+			Assert::AreEqual(Vidminno(stud, 5, "stud.bit"), 60.00);
+		}
+	};
+}
